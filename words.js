24fb891ef724bcc08d6ab3a595f27312f36aec62
DevSeb <seb@kwiefreiheit.com>
2022-12-04 11:52:27 +0100

    created a table, a function to append a new table row and a function to insert a new word with german, korean, ex in de and ex in ko. All hard coded. Needs to be implemented into a map function.

---
+++ words.js
@@ -0,0 +1,29 @@
+// FIND conventional structure of dictionary files
+// JSON or JavasScript Object?
+
+const wordList = [
+    {
+        de: "rausfliegen",
+        ko: "짤리다",
+        ex: [
+                {
+                    de: "Martin ist rausgeflogen.",
+                    ko: "마틴이 짤렸다."
+            }
+        ]
+
+    },
+    {
+        de: "rausfliegen",
+        ko: "짤리다",
+        ex: [
+                {
+                    de: "Martin ist rausgeflogen.",
+                    ko: "마틴이 짤렸다."
+                }
+        ]
+
+    }
+]
+
+export default wordList;
